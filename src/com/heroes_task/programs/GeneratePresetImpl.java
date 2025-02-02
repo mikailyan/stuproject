e3f6e3745dc8cbe2b1ecc836546eb5d7f5f580cb
Микаил Булдаков <mika-buldakov@mail.ru>
2025-02-02 20:52:38 +0300

    final result

---
+++ src/com/heroes_task/programs/GeneratePresetImpl.java
@@ -0,0 +1,80 @@
+package com.heroes_task.programs;
+
+import com.battle.heroes.army.Army;
+import com.battle.heroes.army.Unit;
+import com.battle.heroes.army.programs.GeneratePreset;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class GeneratePresetImpl implements GeneratePreset {
+
+    // Сложность: Сортировка - O(n log n), Выбор юнитов - O(n). В итоге ~ O(n log n)
+    @Override
+    public Army generate(List<Unit> unitList, int maxPoints) {
+        Army computerArmy = new Army();
+        List<Unit> selectedUnits = new ArrayList<>();
+
+        sortUnitsByEffectiveness(unitList);
+
+        int currentPoints = 0;
+
+        for (Unit unit : unitList) {
+            int unitsToAdd = calculateMaxUnitsToAdd(unit, maxPoints, currentPoints);
+            addUnitsToArmy(unit, unitsToAdd, selectedUnits);
+            currentPoints += unitsToAdd * unit.getCost();
+        }
+
+        assignCoordinates(selectedUnits);
+        computerArmy.setUnits(selectedUnits);
+        computerArmy.setPoints(currentPoints);
+        return computerArmy;
+    }
+
+    private void sortUnitsByEffectiveness(List<Unit> units) {
+        units.sort(Comparator.comparingDouble(unit -> -((double) (unit.getBaseAttack() + unit.getHealth()) / unit.getCost())));
+    }
+
+    private int calculateMaxUnitsToAdd(Unit unit, int maxPoints, int currentPoints) {
+        return Math.min(11, (maxPoints - currentPoints) / unit.getCost());
+    }
+
+    private void addUnitsToArmy(Unit unit, int unitsToAdd, List<Unit> selectedUnits) {
+        for (int i = 0; i < unitsToAdd; i++) {
+            Unit newUnit = createNewUnit(unit, i);
+            selectedUnits.add(newUnit);
+        }
+    }
+
+    private Unit createNewUnit(Unit unit, int index) {
+        Unit newUnit = new Unit(unit.getName(), unit.getUnitType(), unit.getHealth(),
+            unit.getBaseAttack(), unit.getCost(), unit.getAttackType(),
+            unit.getAttackBonuses(), unit.getDefenceBonuses(), -1, -1);
+        newUnit.setName(unit.getUnitType() + " " + index);
+        return newUnit;
+    }
+
+    private void assignCoordinates(List<Unit> units) {
+        Set<String> occupiedCoords = new HashSet<>();
+        Random random = new Random();
+
+        for (Unit unit : units) {
+            assignRandomCoordinates(unit, occupiedCoords, random);
+        }
+    }
+
+    private void assignRandomCoordinates(Unit unit, Set<String> occupiedCoords, Random random) {
+        int coordX, coordY;
+        do {
+            coordX = random.nextInt(3);
+            coordY = random.nextInt(21);
+        } while (occupiedCoords.contains(coordX + "," + coordY));
+        occupiedCoords.add(coordX + "," + coordY);
+        unit.setxCoordinate(coordX);
+        unit.setyCoordinate(coordY);
+    }
+}
---
+++ src/com/heroes_task/programs/SimulateBattleImpl.java
@@ -0,0 +1,41 @@
+package com.heroes_task.programs;
+
+import com.battle.heroes.army.Army;
+import com.battle.heroes.army.Unit;
+import com.battle.heroes.army.programs.PrintBattleLog;
+import com.battle.heroes.army.programs.SimulateBattle;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class SimulateBattleImpl implements SimulateBattle {
+    private PrintBattleLog printBattleLog;
+    //  Сложность: O(a * b). (a - количество юнитов в армии игрока, b - количество юнитов в армии компьютера)
+    @Override
+    public void simulate(Army playerArmy, Army computerArmy) throws InterruptedException {
+        Set<Unit> playerUnits = new HashSet<>(playerArmy.getUnits());
+        Set<Unit> computerUnits = new HashSet<>(computerArmy.getUnits());
+
+        while (!playerUnits.isEmpty() && !computerUnits.isEmpty()) {
+            executeAttacks(playerUnits, computerUnits);
+            executeAttacks(computerUnits, playerUnits);
+        }
+    }
+
+    private void executeAttacks(Set<Unit> attackingUnits, Set<Unit> defendingUnits) throws InterruptedException {
+        Iterator<Unit> iterator = attackingUnits.iterator();
+        while (iterator.hasNext()) {
+            Unit attackingUnit = iterator.next();
+            if (!attackingUnit.isAlive()) {
+                iterator.remove();
+                continue;
+            }
+
+            Unit target = attackingUnit.getProgram().attack();
+            if (target != null) {
+                printBattleLog.printBattleLog(attackingUnit, target);
+            }
+        }
+    }
+}
